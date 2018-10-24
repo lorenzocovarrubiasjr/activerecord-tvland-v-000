25a930d3422d5264da1ffc8661495058907d23c1
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-24 19:41:06 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def build_network(network_call_letters)
-      self.network = Network.find_or_create_by(call_letters: network_call_letters)
+      network_abr = Network.find_or_create_by(call_letters: network_call_letters)
+      self.network = network_abr
       binding.pry
    end 
    
