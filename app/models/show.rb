8ca1ecef614c045a97fa878c50c357a140be147a
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-24 19:18:24 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
   
+  has_one :network 
+  has_many :characters 
+  has_many :actors, through: :characters
+  
+  def build_network(network_name)
+      self.network_id = Network.find_or_create_by(network_name)
+   end 
+   
+   def network 
+     
+   end 
+  
 end
