c5692dbdacd50ee66a0e35c7f04973c7ef3fba38
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-24 19:36:48 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,6 +6,7 @@ class Show < ActiveRecord::Base
   
   def build_network(network_call_letters)
       self.network = Network.find_or_create_by(call_letters: network_call_letters)
+      binding.pry
    end 
    
   
